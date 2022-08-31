b259c48325ea1dd5c0e3ddbedeabfcfebf99ac7f
Natalia Sokolova <solta@inbox.ru>
2022-08-31 09:33:52 +0300

    auth problem

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -5,7 +5,7 @@ module.exports = (req, res, next) => {
   const { authorization } = req.headers;
 
   if (!authorization || !authorization.startsWith('Bearer')) {
-    throw new Unauthorized('Необходима авторизация');
+    throw new Unauthorized(`Необходима авторизация 1 ${authorization}`);
   }
 
   const token = authorization.replace('Bearer ', '');
@@ -14,7 +14,7 @@ module.exports = (req, res, next) => {
   try {
     payload = jwt.verify(token, 'some-secret-key');
   } catch (err) {
-    throw new Unauthorized('Необходима авторизация');
+    throw new Unauthorized(`Необходима авторизация 2 ${token}`);
   }
 
   req.user = payload;
